591d4251917adcaf732f9e3ba7a92361ce4900f0
Joshua <rowley.joshua@outlook.com>
2019-05-23 14:49:22 +1000

    Added DrawingCommand class

--- src/DrawingCommand.java
+++ src/DrawingCommand.java
@@ -1,3 +1,23 @@
-public class DrawingCommand {
+import java.util.ArrayList;
 
+public interface DrawingCommand {
+    DrawCommands type();
+
+    ArrayList<Double> coordinates();
+
+    String property();
+
+    enum DrawCommands {
+        LINE("LINE"), PEN("PEN"), FILL("FILL"), PLOT("PLOT");
+
+        private String string;
+
+        DrawCommands(String string) {
+            this.string = string;
+        }
+
+        public String price() {
+            return string;
+        }
+    }
 }
