69f802173514bd339910190de3ac7d2f11b8459f
lena <lenacstockwell@gmail.com>
2019-05-24 21:23:09 +1000

    Big GUI overhaul

--- src/Assignment/GUI.java
+++ src/Assignment/GUI.java
@@ -1,30 +1,126 @@
 package Assignment;
 
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.*;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.Insets;
-import javax.swing.JButton;
-
 
 public class GUI {
 
-    public static void main(String[] args) {
+    JButton clearBtn, blackBtn, blueBtn, greenBtn, redBtn, magentaBtn,
+            orangeBtn, yellowBtn, plotBtn, lineBtn, rectangleBtn, ellipseBtn,
+            polygonBtn, fillBtn;
+    Paint drawArea;
+    ActionListener actionListener = new ActionListener() {
 
-        SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                createAndShowGUI();
+        public void actionPerformed(ActionEvent e) {
+            if (e.getSource() == clearBtn) {
+                drawArea.clear();
+            } else if (e.getSource() == blackBtn) {
+                drawArea.black();
+            } else if (e.getSource() == blueBtn) {
+                drawArea.blue();
+            } else if (e.getSource() == greenBtn) {
+                drawArea.green();
+            } else if (e.getSource() == redBtn) {
+                drawArea.red();
+            } else if (e.getSource() == magentaBtn) {
+                drawArea.magenta();
+            } else if (e.getSource() == orangeBtn) {
+                drawArea.orange();
+            } else if (e.getSource() == yellowBtn) {
+                drawArea.yellow();
             }
-        });
+        }
+    };
+
+    public static void main(String[] args) {
+        new GUI().show();
     }
 
-    private static void createAndShowGUI() {
-        JFrame f = new JFrame("Swing Assignment.Paint Demo");
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        f.add(new MyPanel());
-        f.setSize(800,800);
-        f.setVisible(true);
-        f.add(new MyPanel());
+    public void show() {
+        // create main frame
+        JFrame frame = new JFrame("Swing Paint");
+        Container content = frame.getContentPane();
+        // set layout on content pane
+        content.setLayout(new BorderLayout());
+        // create draw area
+        drawArea = new Paint();
+
+        // add to content pane
+        content.add(drawArea, BorderLayout.CENTER);
+        // create controls to apply colors and call clear feature
+        JPanel controls = new JPanel();
+
+        // create buttons
+        clearBtn = new JButton("Clear");
+        clearBtn.addActionListener(actionListener);
+        blackBtn = new JButton("Black");
+        blackBtn.addActionListener(actionListener);
+        blackBtn.setBackground(Color.BLACK);
+        blueBtn = new JButton("Blue");
+        blueBtn.setBackground(Color.BLUE);
+        blueBtn.addActionListener(actionListener);
+        greenBtn = new JButton("Green");
+        greenBtn.addActionListener(actionListener);
+        greenBtn.setBackground(Color.GREEN);
+        redBtn = new JButton("Red");
+        redBtn.addActionListener(actionListener);
+        redBtn.setBackground(Color.RED);
+        magentaBtn = new JButton("Magenta");
+        magentaBtn.addActionListener(actionListener);
+        magentaBtn.setBackground(Color.MAGENTA);
+        orangeBtn = new JButton("Orange");
+        orangeBtn.addActionListener(actionListener);
+        orangeBtn.setBackground(Color.ORANGE);
+        yellowBtn = new JButton("Yellow");
+        yellowBtn.addActionListener(actionListener);
+        yellowBtn.setBackground(Color.YELLOW);
+
+        // add buttons to panel
+        controls.add(blackBtn);
+        controls.add(magentaBtn);
+        controls.add(redBtn);
+        controls.add(orangeBtn);
+        controls.add(yellowBtn);
+        controls.add(greenBtn);
+        controls.add(blueBtn);
+        controls.add(clearBtn);
+
+        // add to top of content pane
+        content.add(controls, BorderLayout.NORTH);
+
+        // create panel to switch between tools
+        JPanel tools = new JPanel();
+
+        // create buttons for tools
+        plotBtn = new JButton("Plot");
+        plotBtn.addActionListener(actionListener);
+        lineBtn = new JButton("Line");
+        lineBtn.addActionListener(actionListener);
+        rectangleBtn = new JButton("Rectangle");
+        rectangleBtn.addActionListener(actionListener);
+        ellipseBtn = new JButton("Ellipse");
+        ellipseBtn.addActionListener(actionListener);
+        polygonBtn = new JButton("Polygon");
+        polygonBtn.addActionListener(actionListener);
+        fillBtn = new JButton("Fill");
+        fillBtn.addActionListener(actionListener);
+
+
+        // add buttons to tools panel
+        tools.add(plotBtn);
+        tools.add(lineBtn);
+        tools.add(rectangleBtn);
+        tools.add(ellipseBtn);
+        tools.add(polygonBtn);
+        tools.add(fillBtn);
+
+        // add to bottom of content pane
+        content.add(tools, BorderLayout.SOUTH);
+
+
+        // add menu bar
         JMenuBar mb=new JMenuBar();
         JMenu file;
         JMenu edit;
@@ -39,6 +135,12 @@ public class GUI {
         edit.add(undo); edit.add(redo);
         mb.add(file);
         mb.add(edit);
-        f.setJMenuBar(mb);
+        frame.setJMenuBar(mb);
+
+        frame.setSize(600, 600);
+        // can close frame
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // show the swing paint result
+        frame.setVisible(true);
     }
 }
--- src/Assignment/Paint.java
+++ src/Assignment/Paint.java
@@ -1,105 +1,106 @@
 package Assignment;
 
-import javax.swing.*;
-import java.awt.*;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.RenderingHints;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
 
-class MyPanel extends JPanel {
+import javax.swing.JComponent;
 
-    RedSquare redSquare = new RedSquare();
+public class Paint extends JComponent {
 
-    public MyPanel() {
-        setBorder(BorderFactory.createLineBorder(Color.black));
+    // Image in which we're going to draw
+    private Image image;
+    // Graphics2D object ==> used to draw on
+    private Graphics2D g2;
+    // Mouse coordinates
+    private int currentX, currentY, oldX, oldY;
 
-        addMouseListener(new MouseAdapter(){
-            public void mousePressed(MouseEvent e){
-                moveSquare(e.getX(),e.getY());
+    public Paint() {
+        setDoubleBuffered(false);
+        addMouseListener(new MouseAdapter() {
+            public void mousePressed(MouseEvent e) {
+                // save coord x,y when the mouse is pressed
+                oldX = e.getX();
+                oldY = e.getY();
             }
         });
 
-        addMouseMotionListener(new MouseAdapter(){
-            public void mouseDragged(MouseEvent e){
-                moveSquare(e.getX(),e.getY());
+        addMouseMotionListener(new MouseMotionAdapter() {
+            public void mouseDragged(MouseEvent e) {
+                // coord x,y when dragging mouse
+                currentX = e.getX();
+                currentY = e.getY();
+
+                if (g2 != null) {
+                    // draw line if g2 context is not null
+                    g2.drawLine(oldX, oldY, currentX, currentY);
+                    // refresh draw area to repaint
+                    repaint();
+                    // store current coords x,y as olds x,y
+                    oldX = currentX;
+                    oldY = currentY;
+                }
             }
         });
-
     }
 
-    private void moveSquare(int x, int y){
-
-        // Current square state, stored as final variables
-        // to avoid repeat invocations of the same methods.
-        final int CURR_X = redSquare.getX();
-        final int CURR_Y = redSquare.getY();
-        final int CURR_W = redSquare.getWidth();
-        final int CURR_H = redSquare.getHeight();
-        final int OFFSET = 1;
-
-        if ((CURR_X!=x) || (CURR_Y!=y)) {
-
-            // The square is moving, repaint background
-            // over the old square location.
-            //repaint(CURR_X,CURR_Y,CURR_W+OFFSET,CURR_H+OFFSET);
-
-            // Update coordinates.
-            redSquare.setX(x);
-            redSquare.setY(y);
-
-            // Repaint the square at the new location.
-            repaint(redSquare.getX(), redSquare.getY(),
-                    redSquare.getWidth()+OFFSET,
-                    redSquare.getHeight()+OFFSET);
+    protected void paintComponent(Graphics g) {
+        if (image == null) {
+            // image to draw null ==> we create
+            image = createImage(getSize().width, getSize().height);
+            g2 = (Graphics2D) image.getGraphics();
+            // enable antialiasing
+            g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+            // clear draw area
+            clear();
         }
+
+        g.drawImage(image, 0, 0, null);
     }
 
-    public Dimension getPreferredSize() {
-        return new Dimension(250,200);
+    public void clear() {
+        g2.setPaint(Color.white);
+        // draws white on entire draw area to clear
+        g2.fillRect(0, 0, getSize().width, getSize().height);
+        g2.setPaint(Color.black);
+        repaint();
     }
 
-    public void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        g.drawString("This is my custom Panel!",10,20);
-
-        redSquare.paintSquare(g);
-    }
-}
-
-class RedSquare{
-
-    private int xPos = 50;
-    private int yPos = 50;
-    private int width = 20;
-    private int height = 20;
-
-    public void setX(int xPos){
-        this.xPos = xPos;
+    public void red() {
+        // apply red colour
+        g2.setPaint(Color.red);
     }
 
-    public int getX(){
-        return xPos;
+    public void black() {
+        //apply black colour
+        g2.setPaint(Color.black);
     }
 
-    public void setY(int yPos){
-        this.yPos = yPos;
+    public void magenta() {
+        //apply magenta colour
+        g2.setPaint(Color.magenta);
     }
 
-    public int getY(){
-        return yPos;
+    public void green() {
+        //apply green colour
+        g2.setPaint(Color.green);
     }
 
-    public int getWidth(){
-        return width;
+    public void blue() {
+        //apply blue colour
+        g2.setPaint(Color.blue);
     }
-
-    public int getHeight(){
-        return height;
+    public void orange() {
+        //apply orange colour
+        g2.setPaint(Color.orange);
     }
-
-    public void paintSquare(Graphics g){
-        g.setColor(Color.RED);
-        g.fillRect(xPos,yPos,width,height);
-        g.setColor(Color.RED);
-        g.drawRect(xPos,yPos,width,height);
+    public void yellow() {
+        //apply yellow colour
+        g2.setPaint(Color.yellow);
     }
 }
